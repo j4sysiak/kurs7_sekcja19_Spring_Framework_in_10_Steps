d3318c9ad1a9f3948a4bc31fdf4a63f0f0b41698
Jacek <Jacek@BERLIN>
2020-05-30 21:10:06 +0200

    cc

---
+++ kurs7_sekcja19_Spring_Framework_in_10_Steps/src/main/java/pl/jaceksysiak/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,9 @@
+package pl.jaceksysiak.spring.basics.springin5steps;
+
+public class QuickSortAlgorithm implements SortAlgorithm {
+	public int[] sort(int[] numbers) {
+		// Logic for Quick Sort
+		System.out.println("Logic for QuickSortAlgorithm");
+		return numbers;
+	}
+}
